f1e8d8776da14cb3bfa01b310bd9c589a202c22f
adrian-j-velasco <38634263+adrian-j-velasco@users.noreply.github.com>
2018-09-01 08:50:29 -0700

    No delay
    Took out the delay completely. This assumes that the noise from initial read is less than the intended read.

--- GFORCE_test_02.ino
+++ GFORCE_test_02.ino
@@ -10,19 +10,19 @@
 /*
     Accel Variables
 */
-const int adxl_Pin = A1;      //  use analog pin 1 for accelerometer
-int adxl_Raw = 0;             //  output sensor from 0V to 5V
-int g_Max = 250;              //  range of sensor is -250g to 250g
+const int adxl_Pin = A1;               //  use analog pin 1 for accelerometer
+int adxl_Raw = 0;                      //  output sensor from 0V to 5V
+int g_Max = 250;                       //  range of sensor is -250g to 250g
 int g_Min = -250;
-int adxl_Max = 1023;          //  analog has range from 0 to 1023
+int adxl_Max = 1023;                   //  analog has range from 0 to 1023
 int adxl_Min = 0;
-int adxl_Conv, adxl_Adj = 0;  //  2.5V -> 1023/2 -> 0g
-float offset[2] = {1.2567, -0.6};       //  change offsets here, [0] is a multiplier
-//  adxl_Adj = offset[0]*adxl_Conv + offset[1]
-//  Hopefully it doesnt kill itself
-int max_Val = 0;              //  it will only be a positive number
-//  Or the sensor is flipped
-int counter = 0;              //  to reset max_val
+int adxl_Conv, adxl_Adj = 0;           //  2.5V -> 1023/2 -> 0g
+float offset[2] = {1.2567, -0.6};      //  change offsets here, [0] is a multiplier
+                                       //  adxl_Adj = offset[0]*adxl_Conv + offset[1]
+                                       //  Hopefully it doesnt kill itself
+int max_Val = 0;                       //  it will only be a positive number
+                                       //  Or the sensor is flipped
+int counter = 0;                       //  to reset max_val
 /*
     Button stuff
 */
@@ -37,16 +37,16 @@ SevenSegDisp Ssdisp_(8, 6, 10, 12, 13, 7, 9, 5, 4, 3, 2, 11);
 /*
     Function Prototypes
 */
-void Display_serial();        //  Display max val, adxl_Adj, and adxl_Raw
-void Sensor_read();           //  Reads raw data from sensor
-void Get_max();               //  Gets highest value from readings
-void Adjust_raw();            //  Converts raw values to adjusted values
+void Display_serial();                 //  Display max val, adxl_Adj, and adxl_Raw
+void Sensor_read();                    //  Reads raw data from sensor
+void Get_max();                        //  Gets highest value from readings
+void Adjust_raw();                     //  Converts raw values to adjusted values
 
 void setup()
 {
   Button_.Set_bpin(button_Pin);
   Ssdisp_.begin_();
-  //Serial.begin(9600);
+  //Serial.begin(9600);                // Serial slows down the read.
 }
 
 void loop()
@@ -56,20 +56,19 @@ void loop()
   if (Button_.Get_pcounter() % 2 == 0)
   {
     Ssdisp_.Display(max_Val);
-    //Display_serial();   //  take out comment for serial output
+    //Display_serial();                // take out comment for serial output
     counter = 0;
   }
   else
   {
     if(counter == 0)
     {
-      max_Val = 0;
+      max_Val = 0;                     // reset max_Val for next read
     }
-    //delay(300);  //  LOL the lazy easy way lol fuck it
     Sensor_read();
     Adjust_raw();
     Get_max();
-    //Display_serial();   //  take out comment for serial output
+    //Display_serial();                // take out comment for serial output
     counter++;
   }
 }
